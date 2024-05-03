49cd42b24cbfc3666d41bcfe41b70c14dc8f9980
HmF6D43LP9xVE1y7 <info@timott.ch>
2024-05-03 23:01:02 +0200

    058_carousel

---
+++ js/carouselIndividual.js
@@ -0,0 +1,10 @@
+var carousel = new Carousel({
+    elem: 'oubliettes', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval in ms
+    autoplay: false, // autoplay
+})
